30fa77c00c0fada556ee5200e000340739113218
tomasmendietarios <tmendieta@itba.edu.ar>
2025-01-30 10:55:49 -0300

    Instructivo

--- main.py
+++ main.py
@@ -129,6 +129,30 @@ class DatabaseUploader:
 
         return None
 
+class InstructivoApp:
+    def display(self):
+        st.title("Instructivo para usar la Aplicación")
+        st.write("""
+            Bienvenido a la aplicación de análisis de datos. Aquí te explicamos cómo usar cada sección:
+
+            1. **Carga de Datos**: 
+               - Sube dos archivos CSV, uno con datos previos al evento y otro con datos posteriores.
+            
+            2. **Configuración de Filtros**:
+               - Desde la barra lateral, puedes configurar los filtros por fechas, edades, géneros, recomendaciones, etc.
+               - Puedes seleccionar entre diferentes tipos de gráficos.
+
+            3. **Gráficos**:
+               - Los gráficos se generarán automáticamente según los datos y filtros seleccionados.
+               - Puedes ajustar la cantidad de gráficos a mostrar, el tipo de gráfico y el contenido.
+
+            4. **Exportación de Resultados**:
+               - Si es necesario, puedes descargar los datos filtrados desde la sección de Datos.
+
+            5. **Instrucciones adicionales**:
+               - Asegúrate de cargar archivos en el formato correcto.
+               - Si encuentras algún error, revisa que los datos cumplan con el formato esperado.
+        """)
 class DataLoader: 
     def __init__(self):
         self.df = pd.DataFrame()
@@ -1188,97 +1212,95 @@ def main():
     
     # Check for login session state
     if 'logged_in' not in st.session_state:
-        st.session_state.logged_in = False  # Default to not logged in
+        st.session_state.logged_in = False  
 
     # Login Form
     if not st.session_state.logged_in:
         st.title("Inicia sesión para acceder a la aplicación")
         username = st.text_input("Usuario")
         password = st.text_input("Contraseña", type="password")
-        
+
         if st.button("Iniciar sesión"):
             if auth.validate_user(username, password):
-                st.session_state.logged_in = True  # Update session state
-                st.rerun() 
+                st.session_state.logged_in = True
+                st.rerun()  # Re-run the app after successful login
             else:
                 st.error("Usuario o contraseña inválidos")
     else:
-        # If logged in, show the rest of the application
-        st.title("Cargar base de datos")
-        df_all = None  # Initialize df_all as None
-
-        # Manually upload CSV files using the file uploader
-        uploaded_before = st.file_uploader("Cargar la base de datos previo al crash en formato CSV")
-        uploaded_after = st.file_uploader("Cargar la base de datos posterior al crash en formato CSV")
-    
-        data_loader = DataLoader()
-
-        # Check if both files are uploaded
-        if uploaded_before is not None and uploaded_after is not None:
-            with st.spinner("Loading data..."):
-                # Load the data using the DataLoader
-                df_all = data_loader.load_data(uploaded_before, uploaded_after, 'Geo.csv')
-
-            st.success("Base de datos cargada correctamente!")
-
-        # If df_all is still None, warn the user and stop execution
-        if df_all is None:
-            st.warning("Please upload both CSV files to proceed.")
-            return
-
-        num_plots = st.sidebar.slider("Seleccione la cantidad de gráficos que desea ver", min_value=1, max_value=9, value=1, step=1)
-        if num_plots == 1:
-            plots_per_row = 1
-        elif num_plots == 2:
-            plots_per_row = 2
-        else: 
-            plots_per_row = 3
-        plot_count = 0  
+        # Display application tabs
+        tabs = st.tabs(["Instructivo", "Cargar Base de Datos", "Gráficos"])
         
-        while plot_count < num_plots:
-            columns = st.columns(plots_per_row)
-            for col in columns:
-                if plot_count < num_plots:
-                    plot_id = f'plot_{plot_count + 1}'  
-                    st.sidebar.header(f"Gráfico - {plot_count + 1}")  
-                    with st.spinner("Cargando datos y aplicando filtros, por favor espere..."):
-                        streamlit_app = StreamLit(df_all, plot_id)
-                        streamlit_app.sidebar()  
-                        filters = Filters(df_all, plot_id)
-                        filters.choose_filter()
-                        df_filtered = filters.result
-                        df_filtered_antes = filters.result_antes
-                        df_filtered_despues = filters.result_despues
-                        column_order = ['date_recepcion_data', 'user_id', 'SEGUISTE_RECOMENDACIONES', 'days_diff', 'age', 'age_category', 'genero', 'provincia', 'localidad', 'Latitude', 'Longitude', 'RECOMENDACIONES_AJUSTE', 'date_generacion_recomendacion', 'FOTICO_luz_natural_8_15_integrada', 'FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada', 'NOFOTICO_estudios_integrada', 'NOFOTICO_trabajo_integrada', 'NOFOTICO_otra_actividad_habitual_si_no', 'NOFOTICO_cena_integrada', 'HAB_Hora_acostar', 'HAB_Hora_decidir', 'HAB_min_dormir', 'HAB_Soffw', 'NOFOTICO_HAB_alarma_si_no', 'HAB_siesta_integrada', 'HAB_calidad', 'LIB_Hora_acostar', 'LIB_Hora_decidir', 'LIB_min_dormir', 'LIB_Offf', 'LIB_alarma_si_no', 'MEQ_score_total','rec_NOFOTICO_HAB_alarma_si_no', 'rec_FOTICO_luz_natural_8_15_integrada' ,'rec_FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada',	'rec_NOFOTICO_estudios_integrada', 'rec_NOFOTICO_trabajo_integrada', 'rec_NOFOTICO_otra_actividad_habitual_si_no',	'rec_NOFOTICO_cena_integrada',	'rec_HAB_siesta_integrada', 'MEQ_score_total_tipo', 'MSFsc', 'HAB_SDw', 'SJL', 'HAB_SOnw_centrado']
-                        column_order_combinado = ['date_recepcion_data', 'user_id', 'SEGUISTE_RECOMENDACIONES', 'days_diff', 'Periodo' ,'age', 'age_category', 'genero', 'provincia', 'localidad', 'Latitude', 'Longitude', 'RECOMENDACIONES_AJUSTE', 'date_generacion_recomendacion', 'FOTICO_luz_natural_8_15_integrada', 'FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada', 'NOFOTICO_estudios_integrada', 'NOFOTICO_trabajo_integrada', 'NOFOTICO_otra_actividad_habitual_si_no', 'NOFOTICO_cena_integrada', 'HAB_Hora_acostar', 'HAB_Hora_decidir', 'HAB_min_dormir', 'HAB_Soffw', 'NOFOTICO_HAB_alarma_si_no', 'HAB_siesta_integrada', 'HAB_calidad', 'LIB_Hora_acostar', 'LIB_Hora_decidir', 'LIB_min_dormir', 'LIB_Offf', 'LIB_alarma_si_no', 'MEQ_score_total','rec_NOFOTICO_HAB_alarma_si_no', 'rec_FOTICO_luz_natural_8_15_integrada' ,'rec_FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada',	'rec_NOFOTICO_estudios_integrada', 'rec_NOFOTICO_trabajo_integrada', 'rec_NOFOTICO_otra_actividad_habitual_si_no',	'rec_NOFOTICO_cena_integrada',	'rec_HAB_siesta_integrada', 'MEQ_score_total_tipo', 'MSFsc', 'HAB_SDw', 'SJL', 'HAB_SOnw_centrado']
-                        df_all = df_all[column_order]
-                        df_filtered = df_filtered[column_order]
-            
-                        df_all = df_all.sort_values(by=['user_id', 'date_recepcion_data'], ascending=[True, True])
-                        df_filtered = df_filtered.sort_values(by=['user_id', 'date_recepcion_data'], ascending=[True, True])
-                        
-                        df_filtered_antes.loc[:, 'Periodo'] = 'Antes'
-                        df_filtered_despues.loc[:, 'Periodo'] = 'Después'
-                        df_filtered_antes = df_filtered_antes.reset_index(drop=True)
-                        df_filtered_despues = df_filtered_despues.reset_index(drop=True)
-                        df_combinado = pd.concat([df_filtered_antes, df_filtered_despues], ignore_index=False)
-                        df_combinado = df_combinado.reset_index(drop=True)
-                        df_combinado = df_combinado[column_order_combinado]
-                        df_combinado = df_combinado.sort_values(by=['user_id', 'date_recepcion_data'], ascending=[True, True])
-                        with col:  
-                            if st.session_state['datos_' + plot_id] == False:                    
-                                
-                                st.title('Datos')
-                                if st.session_state['ambas_antes_despues_' + plot_id] == 'Antes vs Después':   
-                                    st.write(f'Cantidad : {len(df_combinado)}')  
-                                    st.write(df_combinado)
-                                else:
-                                    st.write(f'Cantidad : {len(df_filtered)}')  
-                                    st.write(df_filtered)  
-                            plot_generator = PlotGenerator(df_filtered, df_combinado, plot_id) 
-                            plot_generator.choose_plot()  
+        # Instructivo tab
+        with tabs[0]:
+            instructivo_app = InstructivoApp()
+            instructivo_app.display()
 
-                        plot_count += 1  
+        # Data Upload tab
+        with tabs[1]:
+            st.title("Cargar base de datos")
+            data_loader = DatabaseUploader()
+            uploaded_before, uploaded_after = data_loader.upload_data()
+
+            if uploaded_before and uploaded_after:
+                with st.spinner("Cargando datos..."):
+                    data_loader_app = DataLoader()
+                    df_all = data_loader_app.load_data(uploaded_before, uploaded_after, 'Geo.csv')
+                st.success("Base de datos cargada correctamente!")
+            else:
+                st.warning("Por favor, sube ambos archivos CSV para continuar.")
+                df_all = None  # Ensure df_all is set to None
+
+        # Graphs tab
+        with tabs[2]:
+            st.title("Generación de Gráficos")
+            if df_all is None:
+                st.header("Primero cargue la base de datos")
+                return
+
+            # Select number of plots
+            num_plots = st.sidebar.slider("Seleccione la cantidad de gráficos que desea ver", min_value=1, max_value=9, value=1, step=1)
+            plots_per_row = 1 if num_plots == 1 else (2 if num_plots == 2 else 3)
+
+            # Generate plots
+            plot_count = 0  
+            while plot_count < num_plots:
+                columns = st.columns(plots_per_row)
+                for col in columns:
+                    if plot_count < num_plots:
+                        plot_id = f'plot_{plot_count + 1}'  
+                        st.sidebar.header(f"Gráfico - {plot_count + 1}")  
+                        
+                        with st.spinner("Cargando datos y aplicando filtros, por favor espere..."):
+                            # Initialize StreamLit and Filters
+                            streamlit_app = StreamLit(df_all, plot_id)
+                            streamlit_app.sidebar()
+                            filters = Filters(df_all, plot_id)
+                            filters.choose_filter()
+
+                            # Get filtered data
+                            df_filtered = filters.result
+                            df_filtered_antes = filters.result_antes
+                            df_filtered_despues = filters.result_despues
+
+                            # Prepare combined data for comparison plots
+                            df_filtered_antes['Periodo'] = 'Antes'
+                            df_filtered_despues['Periodo'] = 'Después'
+                            df_combinado = pd.concat([df_filtered_antes, df_filtered_despues], ignore_index=True)
+                            df_combinado.sort_values(by=['user_id', 'date_recepcion_data'], inplace=True)
+
+                            # Display data or plots
+                            with col:
+                                if not st.session_state['datos_' + plot_id]:
+                                    st.title('Datos')
+                                    data_to_display = df_combinado if st.session_state['ambas_antes_despues_' + plot_id] == 'Antes vs Después' else df_filtered
+                                    st.write(f'Cantidad: {len(data_to_display)}')
+                                    st.write(data_to_display)
+
+                                # Generate the plot
+                                plot_generator = PlotGenerator(df_filtered, df_combinado, plot_id)
+                                plot_generator.choose_plot()
+
+                        plot_count += 1  # Move to the next plot
 
 main()
 
